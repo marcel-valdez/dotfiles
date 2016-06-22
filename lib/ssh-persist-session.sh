5a7e4b19c86cca9558546b16ad6918215cad10c5
Marcel Valdez <marcelvaldez@google.com>
2016-06-21 17:47:32 -0700

    Cache result of ssh-add -l instead of refetching

--- lib/ssh-persist-session.sh
+++ lib/ssh-persist-session.sh
@@ -6,24 +6,48 @@ env=~/.ssh/agent.env
 # Note: Don't bother checking SSH_AGENT_PID. It is not used by
 # SSH itself, and it might even be incorrect.
 # (For example, when using agent-forwarding over SSH)
+
+clear_ssh_add_l_cache() {
+  unset SSH_KEYS
+  unset SSH_ADD_STATUS
+}
+
+cache_ssh_add_l() {
+  if [ "$SSH_KEYS" == "" ]; then
+    SSH_KEYS=$(ssh-add -l)
+    SSH_ADD_STATUS=$?
+  fi
+}
+
 agent_is_running() {
   if [ "$SSH_AUTH_SOCK" ]; then
+    cache_ssh_add_l
     # ssh-add returns:
     # 0 = agent running, has keys
     # 1 = agent running, no keys
     # 2 = agent not running
-    ssh-add -l >/dev/null 2>&1 || [ $? -eq 1 ]
+    if [ $SSH_ADD_STATUS -eq 1 ]; then
+      false
+    else
+      true
+    fi
   else
     false
   fi
 }
 
 agent_has_keys() {
-  ssh-add -l > /dev/null 2>&1
+  cache_ssh_add_l
+  if [ "$SSH_KEYS" == "" ]; then
+    false
+  else
+    true
+  fi
 }
 
 agent_has_home_keys() {
-  ssh-add -l | grep "$HOME/.ssh/" > /dev/null 2>&1
+  cache_ssh_add_l
+  echo "$SSH_KEYS" | grep "$HOME/.ssh/" > /dev/null 2>&1
 }
 
 agent_load_env() {
@@ -35,19 +59,24 @@ agent_start() {
   source "$env" > /dev/null
 }
 
+
 if ! agent_is_running; then
-  echo "Loading SSH environment."
+  echo "Loading SSH environment"
   agent_load_env
 fi
 
 if ! agent_is_running; then
-  echo "Starting ssh-agent."
+  echo "Starting ssh-agent"
   agent_start
   ssh-add
-elif ! agent_has_keys; then
-  ssh-add
-elif ! agent_has_home_keys; then
-  ssh-add
+else
+  if ! agent_has_keys; then
+    echo "Adding all keys to ssh-agent"
+    ssh-add
+  elif ! agent_has_home_keys; then
+    echo "Adding keys in $HOME/.ssh/ dir to ssh-agent"
+    ssh-add
+  fi
 fi
 
 unset env
