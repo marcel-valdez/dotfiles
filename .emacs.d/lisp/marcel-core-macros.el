fc49dcb6d49142c94828eb5cf302a3be50a0a728
Marcel Valdez <marcelvaldez@google.com>
2016-11-03 11:32:10 -0700

    emacs: with-library now fails if the body fails to execute

--- .emacs.d/lisp/marcel-core-macros.el
+++ .emacs.d/lisp/marcel-core-macros.el
@@ -1,10 +1,17 @@
 (provide 'marcel-core-macros)
 
 (defmacro with-library (symbol &rest body)
-  `(condition-case nil
-       (progn
-         (require ',symbol)
-         ,@body)
-     (error (message (format "I guess we don't have %s available." ',symbol))
-            nil)))
+  "Executes the &rest body if it is able to successfully 'require the given
+symbol. If the symbol cannot be 'require'd, then the &rest body is not executed
+and no error is raised. If the &rest body fails during execution, the error is
+allowed to bubble up, it is not caught."
+  `(when
+       (condition-case err
+           (progn (require ',symbol) t)
+         (error
+          (message
+           (format "Error while requiring package %s\n%s" ',symbol err))
+          nil))
+     ,@body))
+
 (put 'with-library 'lisp-indent-function 1)
