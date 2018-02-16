877068fc2bd3ba631858cdf2b0a840c4937f736c
Marcel Valdez <marcelvaldez@google.com>
2018-02-16 01:26:18 -0800

    Make the clipboard daemon paste to all available X Displays

--- bin/clipboard-daemon.sh
+++ bin/clipboard-daemon.sh
@@ -8,6 +8,16 @@ if [ ${NUM} -gt 0 ]; then
 fi
 
 while [ true ]; do
-  nc -l ${HOST} ${PORT} | xclip -selection clipboard
+  displays=$(ls /tmp/.X11-unix/ | sed 's/X//g')
+  readarray -t displays_array <<< ${displays}
+  paste_buffer=$(nc -l "${HOST}" "${PORT}") # wait for next paste to clipboard
+  for display in ${displays_array[@]} # paste to all available displays
+  do
+    echo ${paste_buffer} | xclip -selection clipboard --display ":${display}"
+    if [[ $? -gt 0 ]]; then
+      echo "$(today) $(date '+%H:%M:%S'): Failed to copy contents to \
+remote SSH clipboard at display :${display}" &>> /tmp/clipboard-daemon.log
+    fi
+  done
   echo "$(today) $(date '+%H:%M:%S'): Copied remote SSH clipboard." &>> /tmp/clipboard-daemon.log
 done
