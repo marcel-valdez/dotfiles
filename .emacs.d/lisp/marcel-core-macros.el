a17d633ccab8614019a6278eb52effed9c819673
Marcel Valdez <marcelvaldez@google.com>
2018-03-09 13:58:25 -0800

    Lint clean of marcel-core-macros

--- .emacs.d/lisp/marcel-core-macros.el
+++ .emacs.d/lisp/marcel-core-macros.el
@@ -1,10 +1,15 @@
-(provide 'marcel-core-macros)
+;;; marcel-core-macros.el --- Summary
+;;; This file contains completely package independent core macros for my .emacs config
 
+;;; Commentary:
+;;; Use this function to load packages safely.
+
+;;; Code:
 (defmacro with-library (symbol &rest body)
-  "Executes the &rest body if it is able to successfully 'require the given
-symbol. If the symbol cannot be 'require'd, then the &rest body is not executed
-and no error is raised. If the &rest body fails during execution, the error is
-allowed to bubble up, it is not caught."
+  "If it can successfully 'require the given SYMBOL, then execute the BODY.
+If the SYMBOL cannot be 'require'd, then the BODY is not executed and no error
+is raised.
+If the BODY fails during execution, the error is allowed to bubble up, it is not caught."
   `(when
        (condition-case err
            (progn (require ',symbol) t)
@@ -15,3 +20,6 @@ allowed to bubble up, it is not caught."
      ,@body))
 
 (put 'with-library 'lisp-indent-function 1)
+
+(provide 'marcel-core-macros)
+;;; marcel-core-macros.el ends here
