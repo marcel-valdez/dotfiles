c886b958398e00e632083a95534d3102147c41b3
Marcel Valdez <marcelvaldez@fb.com>
2019-12-13 18:37:03 -0800

    emacs/wsl-copy: Remove it as a mode, only functions.

--- .emacs.d/lisp/wsl-copy.el
+++ .emacs.d/lisp/wsl-copy.el
@@ -1,21 +1,21 @@
 ;;; wsl-copy.el --- Summary
-;;; WSL Linux functionality.
+;;; WSL Linux Clipboard functionality.
 
 ;;; Commentary:
-;;; This package contains functions required for Emacs running in Linux under
-;;; the Windows Subsystem for Linux.
+;;; This package contains functions required to copy/paste on the clipboard for
+;;; Emacs running in Linux under the Windows Subsystem for Linux.
 
 ;;; Code:
-;;; TODO: Check if xclip exists.
+;;; TODO: Check if /usr/bin/xclip exists.
 ;;; TODO: Use DISPLAY variable if available, :0 otherwise
 
 
-(defun wsl-copy-check-display ()
+(defun wsl-copy/check-display ()
   "Set the DISPLAY environment variable if not set already."
     (if (not (getenv "DISPLAY"))
         (setenv "DISPLAY" ":0")))
 
-(defun wsl-copy-region (start end)
+(defun wsl-copy/copy-region (start end)
   "Paste the selected region in the host Windows system."
   (interactive "r")
   (wsl-copy-check-display)
@@ -23,23 +23,20 @@
       (call-process-region start end "/usr/bin/xclip"))
   (message "Copied region onto clipboard."))
 
-(defun wsl-copy-kill-ring ()
+(defun wsl-copy/copy-kill-ring ()
   "Paste the Emacs kill ring in the host Windows system."
   (wsl-copy-check-display)
   (process-send-string "/usr/bin/xclip" kill-ring)
   (message "Copied kill ring onto clipboard."))
 
 
-(defun wsl-copy-paste ()
+(defun wsl-copy/paste ()
   "Copy the Windows clipboard onto the current buffer."
   (interactive)
   (wsl-copy-check-display)
   (mapc 'insert (shell-command-to-string "/usr/bin/xclip -o"))
   (message "Pasted clipboard onto buffer."))
 
-(define-minor-mode wsl-copy-mode
-  "Enable copy/paste in emacs under WSL"
-  :lighter "wsl-copy-mode")
 
 (provide 'wsl-copy)
 ;;; wsl-copy.el ends here
