b6aa31fe7c5e215377bfc86ef826e135648105a9
Marcel Valdez <marcel.valdez@live.com>
2021-03-17 14:52:01 -0700

    .wezterm.lua: Remove ctrl+2 binding change scheme to Molokai

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,15 @@
 local wezterm = require 'wezterm';
 
 return {
-   color_scheme = "Darkside",
+   color_scheme = "Molokai",
    enable_tab_bar = false,
    window_decorations = "RESIZE",
    font = wezterm.font_with_fallback(
       { "Source Code Pro", "Consolas", "Terminus" }
    ),
-   adjust_window_size_when_changing_font_size = false
+   adjust_window_size_when_changing_font_size = false,
+   keys = {
+      { key="2", mods="CTRL", action=wezterm.action{SendString="\x00"} },
+      { key="2", mods="SHIFT|CTRL", action="DisableDefaultAssignment" }
+   }
 }
