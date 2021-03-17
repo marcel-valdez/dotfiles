bd8bb552062ee2be281cdcf7efb35507aeb639be
Marcel Valdez <marcel.valdez@live.com>
2021-03-17 09:57:21 -0700

    wezterm: Remove 'always bold' config

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,8 +5,7 @@ return {
    enable_tab_bar = false,
    window_decorations = "RESIZE",
    font = wezterm.font_with_fallback(
-      { "Source Code Pro", "Consolas", "Terminus" },
-      { bold=true }
+      { "Source Code Pro", "Consolas", "Terminus" }
    ),
    adjust_window_size_when_changing_font_size = false
 }
