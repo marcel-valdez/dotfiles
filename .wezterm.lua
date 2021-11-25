8eefe9454adb3199d1878a4bac92e643a7477ac0
Marcel Valdez <_marcel_valdez_@live.com>
2021-11-25 09:31:05 -0800

    wezterm: Add documentation for fonts config in docs

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,17 @@
-local wezterm = require 'wezterm';
+ local wezterm = require 'wezterm';
 
 return {
-   color_scheme = "FirefoxDev", -- Hybrid, JetBrains Darcula, Tomorrow Night, Molokai, Sundried, Japanesque, NightLion v2, FirefoxDev, Wryan, Monokai Remastered, Hardcore, Teerb, Wombat, Operator Mono Dark, OneHalfDark, Ripped Casts, lovelace, Pencil Dark
+   color_scheme = "Hybrid", -- FirefoxDev, Hybrid, JetBrains Darcula, Tomorrow Night, Molokai, Sundried, Japanesque, NightLion v2, FirefoxDev, Wryan, Monokai Remastered, Hardcore, Teerb, Wombat, Operator Mono Dark, OneHalfDark, Ripped Casts, lovelace, Pencil Dark
    enable_tab_bar = false,
    window_decorations = "RESIZE",
-   font = wezterm.font_with_fallback(
-      { "Source Code Pro", "Consolas", "Terminus" }
-   ),
+   -- weight: Thin, ExtraLight, Light, Regular, Medium, SemiBold, Bold, ExtraBold
+   -- stretch: Normal, SemiCondensed, Condensed, ExtraCondensed
+   font = wezterm.font_with_fallback({
+      { family="Noto Sans Mono", weight="Regular", stretch="Normal", italic=false },
+      { family="Source Code Pro"},
+      { family="Consolas"},
+      { family="Terminus"}
+   }),
    adjust_window_size_when_changing_font_size = false,
    keys = {
       { key="2", mods="CTRL", action=wezterm.action{SendString="\x00"} },
