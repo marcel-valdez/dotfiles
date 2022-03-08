14e54de6b5c427a9aea15eab56d6b3bf72cd683f
Marcel Valdez <_marcel_valdez_@live.com>
2022-03-08 11:18:22 -0800

    monitor-tmux-pane: Use tmux hooks to announce on new changes

--- scripts/monitor-tmux-pane.sh
+++ scripts/monitor-tmux-pane.sh
@@ -2,6 +2,12 @@
 
 SCRIPT="$(basename $0)"
 
+function debug {
+  if [[ "${DEBUG}" ]]; then
+    echo "$@"
+  fi
+}
+
 window_name=
 poll_frequency_seq=
 declare -a refresh_script
@@ -9,14 +15,15 @@ declare -a refresh_script
 function usage() {
   cat<<EOF
 
-${SCRIPT} [--help|-h] [--window_name|-w <name>] [--freq|-f <seconds>] [--custom_message|-m <message>] -- refresh_script_args ...
+${SCRIPT} [--help|-h] [--window_name|-w <name>] [--freq|-f <seconds>] [--custom_message|-m <message>] [--no_notify] -- refresh_script_args ...
 
 This is a script to monitor the contents of a tmux window. If the contents change a popup will be shown on the tmux client with a message.
 
 help: Displays this help message
 window_name: The name of the window to monitor.
-custom_message: A custom message to show in the popup dialog. Default: Window <window_name> changed!
 freq: Frequency in seconds to poll for changes.
+custom_message: A custom message to show in the popup dialog. Default: Window <window_name> changed!
+no_notify: Do not notify about new messages, only highlight the window.
 refresh_script_args...: The command to execute before polling, this command may be used to refresh the contents of the pane (e.g. push data to a server and that push may cause a change in the contents of the monitored pane).
 
 Example:
@@ -28,6 +35,7 @@ EOF
 }
 
 USE_DESKTOP_NOTIFY=
+notify_enabled=1
 function parse_args {
   while [ $# -gt 0 ]; do
     arg="$1"
@@ -48,6 +56,9 @@ function parse_args {
         custom_message="$2"
         shift
         ;;
+      --no_notify)
+        notify_enabled=
+        ;;
       --)
         shift
         refresh_script+=("$@")
@@ -64,11 +75,12 @@ function parse_args {
   echo "window_name=${window_name}" >&2
   echo "poll_frequency_sec=${poll_frequency_sec}" >&2
   echo "custom_message=${custom_message}" >&2
+  echo "notify_enabled=${notify_enabled}" >&2
   echo "refresh_script=${refresh_script[@]}" >&2
 }
 
 function get_pane_content {
-  tmux capture-pane -p -t "${window_name}"
+  tmux capture-pane -p -t "${window_name}" | tr --delete '\n' | sed 's|oh=..*\&||g' | sed 's|ohc=..*\&||g'
 }
 
 function refresh_pane {
@@ -87,26 +99,64 @@ function notify_new {
   tmux display-popup -y 1 -w "${popup_width}" -h "${popup_height}" "echo" "-e" "\e[1m\e[5m\033[1;93m${message}"
 }
 
+
+function remove_window_hook {
+  tmux set-hook -t "${window_name}" -u "pane-focus-in[11]"
+}
+
+function set_window_hook {
+  # this will remove the 'changed' mark after the user focuses into the window
+  tmux set-hook -t "${window_name}" "pane-focus-in[11]" \
+    "setw -t '${window_name}' window-status-style default"
+}
+
+function mark_window_changed {
+  # sets a green color on the window status when it changes
+  tmux setw -t "${window_name}" "window-status-style" "fg=#88c388"
+}
+
+function remove_tmux_monitor {
+  tmux setw -t "${window_name}" "monitor-activity" "off"
+}
+
+function cleanup {
+  remove_window_hook
+  exit $1
+}
+
 function monitor_content {
-  local old_content=
   local new_content=
-  old_content="$(get_pane_content)"
+  local oppld_content="$(get_pane_content)"
+  set_window_hook
+  trap cleanup 0 EXIT
+  trap cleanup 1 SIGINT
+
   while true; do
-    focused_window="$(tmux display-message -p '#W')"
+    local focused_window="$(tmux display-message -p '#W')"
     if [[ "${focused_window}" == "${window_name}" ]]; then
       sleep "${poll_frequency_sec}s"
       continue
     fi
     refresh_pane
     sleep "2s"
-    new_content="$(get_pane_content)"
-    if [[ "${old_content}" != "${new_content}" ]]; then
+    # looks like media links change each time the request is made
+    local new_content="$(get_pane_content)"
+    if ! diff  <(echo "${old_content}" ) <(echo "${new_content}") &>/dev/null; then
+      if [[ "${DEBUG}" ]]; then
+        local diff_str=$(diff <(echo "${old_content}") <(echo "${new_content}"))
+        debug "--- diff start ---"
+        debug "${diff_str}"
+        debug "--- diff end ---"
+      fi
       local message="Window ${window_name} changed!"
       if [[ "${custom_message}" ]]; then
-        message="${custom_message}"
+        local message="${custom_message}"
       fi
-      notify_new "${message}"
-      old_content="${new_content}"
+      mark_window_changed
+      if [[ ${notify_enabled} ]]; then
+        notify_new "${message}"
+      fi
+      local old_content="${new_content}"
     fi
     sleep "${poll_frequency_sec}s"
   done
