9d8af046910998db5cb01949697fc23b691c1467
Marcel Valdez Orozco <marcelvaldez@google.com>
2023-08-29 15:49:28 -0700

    fzf: use ${HOME} variable instead of hardcoded path

--- .fzf.bash
+++ .fzf.bash
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/marcelvaldez/.fzf/bin* ]]; then
+if [[ ! "$PATH" == *"${HOME}/.fzf/bin"* ]]; then
   export PATH="${PATH:+${PATH}:}/home/marcelvaldez/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && [[ -f "/home/marcelvaldez/.fzf/shell/completion.bash" ]] && source "/home/marcelvaldez/.fzf/shell/completion.bash" 2> /dev/null
+if [[ $- == *i* ]] && [[ -f "${HOME}/.fzf/shell/completion.bash" ]] && source "${HOME}/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-[[ -f "/home/marcelvaldez/.fzf/shell/key-bindings.bash" ]] && source "/home/marcelvaldez/.fzf/shell/key-bindings.bash"
+[[ -f "${HOME}/.fzf/shell/key-bindings.bash" ]] && source "${HOME}/.fzf/shell/key-bindings.bash"
 
 export FZF_DEFAULT_OPTS='-m '"
 --bind 'ctrl-y:execute-silent(echo -n {} | xclip -sel clip)+abort'
