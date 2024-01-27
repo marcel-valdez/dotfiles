75fef2e5f2f366da29781b11136cbe87b4c391d8
Marcel Valdez Orozco <_marcel_valdez_@outlook.com>
2024-01-26 18:02:16 -0800

    Add scripts to summarize guitar runs results

---
+++ scripts/summarize_guitar_results.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/google/src/head/depot/google3/experimental/users/marcelvaldez/py/runs_summarizer.py "$@"
---
+++ scripts/summarize_presubmit_results.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/google/src/head/depot/google3/experimental/users/marcelvaldez/sh/summarize_presubmit_run.sh "$@"
