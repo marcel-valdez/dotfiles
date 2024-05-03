1b90b680d0d4a3cd0ea3348e75d7661fd8a4a2b3
Marcel Valdez Orozco <marcelvaldez@google.com>
2024-05-03 09:58:03 -0700

    mount notes/gtd: Add compression and caching.

--- scripts/setup_work_environment.sh
+++ scripts/setup_work_environment.sh
@@ -50,11 +50,11 @@ function mount_remote_gcloud_folders {
       fi
       local remote_folder="/usr/local/google/home/${USER}/${folder}"
       echo "Mounting ${GCLOUD_HOST}:${remote_folder} on ${HOME}/${folder}"
-      sshpass -p "$(get_secret)" sshfs -o reconnect "${USER}@${GCLOUD_HOST}:${remote_folder}" "${HOME}/${folder}"
+      sshpass -p "$(get_secret)" sshfs -o compression=yes -o auto_cache -o reconnect "${USER}@${GCLOUD_HOST}:${remote_folder}" "${HOME}/${folder}"
       if [[ $? -ne 0 ]]; then
         echo "Unable to mount folder ${HOME}/${folder}. Unmounting and remounting once." >&2
         umount "${HOME}/${folder}"
-        sshpass -p "$(get_secret)" sshfs -o reconnect "${USER}@${GCLOUD_HOST}:${remote_folder}" "${HOME}/${folder}"
+        sshpass -p "$(get_secret)" sshfs -o compression=yes -o auto_cache -o reconnect "${USER}@${GCLOUD_HOST}:${remote_folder}" "${HOME}/${folder}"
       fi
     fi
     # attempt to list directory contents in order to force reconnect of the SSHFS mount
