c090caea50090f27f53d284eab135c1a5e87f04f
Marcel Valdez Orozco <marcelvaldez@google.com>
2026-03-21 17:25:24 -0700

    scripts/retry_after_key.sh: Flush escape multi-byte characters so it doesn't go into the terminal as [I or otherwise.

--- scripts/retry_after_key.sh
+++ scripts/retry_after_key.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+UNRETRIABLE_ERROR=128
+UNRETRIABLE_SUCCESS=129
+
+source "${HOME}/lib/log_lib.sh" || exit ${UNRETRIABLE_ERROR}
 
 command="$@"
 
@@ -6,7 +10,7 @@ ctrl_c_pressed=
 
 function ctrl_c_handler {
   ctrl_c_pressed=1
-  echo "[ctrl+c] pressed, exiting." >&2
+  log::info "[ctrl+c] pressed, exiting."
   exit 0
 }
 
@@ -14,7 +18,22 @@ trap ctrl_c_handler SIGINT
 
 while true; do
   "$@"
-  echo "Command '$*' finished with exit code: $?" >&2
-  echo  "Press ctrl+c to exit, any other key to retry: '$*'" >&2
+  exit_code=$?
+  log::info "Command '$*' finished with exit code: $?"
+  if [[ ${exit_code} -eq ${UNRETRIABLE_SUCCESS} ]]; then
+    log::info "Code ${exit_code} is UNRETRIABLE_SUCCESS, not retrying."
+    break
+  fi
+  if [[ ${exit_code} -eq ${UNRETRIABLE_ERROR} ]]; then
+    log::fatal "Code ${exit_code} is UNRETRIABLE_ERROR, not retrying."
+    break
+  fi
+
+  echo "Press ctrl+c to exit, any other key to retry: '$*'" >&2
   read -n 1 -s -r >&2
+
+  # 2. Flush any remaining characters (like [I) in the buffer
+  # -t 0.05 waits 50ms for extra chars
+  # -k is not standard, so we use a read loop
+  while read -r -t 0.05; do :; done
 done
